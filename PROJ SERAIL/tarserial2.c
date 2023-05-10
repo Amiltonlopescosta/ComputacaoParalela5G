2d0275109495856e0d6ef877b221a2164f8fcfd3
Amiltonlopescosta <79999632+Amiltonlopescosta@users.noreply.github.com>
2023-05-10 01:05:38 -0300

    Create tarserial2.c

---
+++ PROJ SERAIL/tarserial2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+float factorial(int n)    
+{     
+  int m;       
+  if(n==0){      
+    return 1;     
+  }     
+  float resultado = 1;  
+  for(m=1;m<=n;m++){     
+    resultado *=m;    
+  }      
+  return resultado;    
+}        
+int main ()     
+{   
+  int i, n;   
+  double sum=0.0;      
+  int ln=1000000;  
+  for (i=0; i<ln; i++) sum +=1/ factorial(i);  
+  printf("The value is %lf\n", sum);    
+  return 0;  
+} 
